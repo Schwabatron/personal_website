ea89e70791830a41d979c930107914431d6252a9
Jack Schwabe <schwabejack04@gmail.com>
2024-06-21 22:11:47 -0400

    Adding navbar from the react boostrap library
    adding a navbar and some basic options that i want included in the side.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 function Header() {
-  return <h1 className="text-light bg-dark">Jack Schwabe</h1>;
+  return (
+    <Navbar expand="lg" className="bg-dark">
+      <Container>
+        <Navbar.Brand className="text-light">Jack Schwabe</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="text-light">About Me</Nav.Link>
+            <Nav.Link className="text-light">Projects</Nav.Link>
+            <Nav.Link className="text-light">Contact Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
